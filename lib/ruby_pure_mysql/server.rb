5cde1c1107cbfb60543120cc214d6126a236ccb0
takapg <75102730+takapg@users.noreply.github.com>
2026-05-19 12:28:58 +0900

    Create Server class for RubyPureMysql
    Initial implementation of RubyPureMysql Server class with basic structure.

---
+++ lib/ruby_pure_mysql/server.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'socket'
+
+module RubyPureMysql
+  # TODO: class についての説明を更新してください。
+  class Server
+    def initialize(host: '127.0.0.1', port: 3307)
+      @server = TCPServer.new(host, port)
+    end
+
+    def run
+      # TODO: 実装してください。
+    end
+end
