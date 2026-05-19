edf7b15e7048f3b44f92381ca39e6645e3ab187a
takapg <75102730+takapg@users.noreply.github.com>
2026-05-19 12:18:04 +0900

    Add shared examples for MySQL-compatible server

---
+++ spec/support/shared_mysql_examples.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+RSpec.shared_examples 'a MySQL-compatible server' do |port|
+  let(:client) do
+    Mysql2::Client.new(
+      host: '127.0.0.1',
+      username: 'root',
+      port: port,
+      connect_timeout: 2
+    )
+  end
+
+  after do
+    client&.close
+  rescue StandardError
+    # 接続が既に切れている場合のクローズエラーを無視
+  end
+
+  it 'executes SELECT 1; and returns 1' do
+    results = client.query('SELECT 1;')
+    expect(results.first.values.first).to eq(1)
+  end
+end
